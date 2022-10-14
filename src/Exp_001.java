aae3320f613b004134b0a08ffa270462b4934b23
satish358 <satishmankar745@gmail.com>
2022-10-14 07:56:36 +0530

    init commit

---
+++ src/Exp_001.java
@@ -0,0 +1,30 @@
+public class Exp_001 {
+    public static void main(String[] args){
+        //(Normal natural number)
+        // short (16bit / 2Byte), -2^16 to (2^16)-1
+        // int, (32bit / 4Byte) -2^32 to (2^32)-1
+        // long (64bit / 8Byte) -2^64 to (2^64)-1
+
+        // VARIABLE_TYPE VARIABLE_NAME = VALUE;
+        short a = 10;
+        short b = -134;
+        int aInt = 111;
+        int bInt = -32423;
+        long aLong = 324234234;
+        long bLong = -89343434;
+
+        // Floating Numbers
+        // float, double
+        float aFloat = 1034.3434f;
+        double aDouble = 1234.2341;
+
+        // boolean (true, false)
+        boolean isValid = false;
+
+        // worlds
+        // char, String
+        char x = 'x';
+        String str = "Hello, this is first app";
+
+    }
+}
